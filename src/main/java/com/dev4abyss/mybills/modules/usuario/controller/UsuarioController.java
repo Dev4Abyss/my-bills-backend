cdabc00d15172347d8cf84fd70559726d2736d65
Márcio Lôbo <cast.mlobo.utic@sebrae.com.br>
2022-08-10 20:46:57 -0300

    configurando swagger

--- src/main/java/com/dev4abyss/mybills/modules/usuario/controller/UsuarioController.java
+++ src/main/java/com/dev4abyss/mybills/modules/usuario/controller/UsuarioController.java
@@ -2,23 +2,30 @@ package com.dev4abyss.mybills.modules.usuario.controller;
 
 import com.dev4abyss.mybills.modules.usuario.entity.Usuario;
 import com.dev4abyss.mybills.modules.usuario.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "Usuários", description = "Recursos Sobre Usuários")
 @RequestMapping("/v1/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Lista os usuários da api")
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         return ResponseEntity.ok(usuarioService.listarTudo());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.save(usuario));
     }
 }
