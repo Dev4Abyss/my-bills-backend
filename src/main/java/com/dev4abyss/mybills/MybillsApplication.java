1700be2c53d7f744bc5fa4d155f14f9038d1ba01
Márcio Lôbo <cast.mlobo.utic@sebrae.com.br>
2022-08-10 19:14:03 -0300

    estrutura incial do projeto

---
+++ src/main/java/com/dev4abyss/mybills/MybillsApplication.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.mybills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev4abyss/mybills/MybillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.mybills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
